9c93813315c45b49cac35adf103a8da3d51bdc2e
Tobias Haagen Michaelsen <tobias.michaelsen@karnovgroup.com>
2015-06-19 15:45:46 +0200

    Use inline template
    ... to avoid large chunk of text in the middle of the code.

--- client.rb
+++ client.rb
@@ -2,6 +2,7 @@
 require "savon"
 require "json"
 require "sinatra"
+require "tilt/erb"
 
 helpers do
   attr_accessor :client
@@ -23,47 +24,7 @@ helpers do
 end
 
 get '/' do
-  <<-EOC
-    <!DOCTYPE HTML>
-    <title>
-      RESTful gateway for DSB Labs
-    </title>
-    <h1>
-      RESTful gateway for <a href="http://www.dsb.dk/dsb-labs/webservice-stationsafgange/">DSB Labs – Webservice: Stationsafgange</a>
-    </h1>
-    <p>
-      <em>All data is returned as JSON</em>
-    </p>
-    <p>
-      You might want to see the <a href="stations">list of stations</a>.
-    </p>
-    <form action="queue">
-      <p>
-        Or find departures etc. for a given station.
-        <label>Station UIC: <input type="text" name="uic"></label>
-        <input type="submit">
-      </p>
-    </form>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js"></script>
-    <script>
-      var stations = new Bloodhound({
-        identify: function(station) { return station.uic; },
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        prefetch: '/stations'
-      });
-
-      $('input[name="uic"]').typeahead(null, {
-        display: 'uic',
-        name: 'stations',
-        source: stations,
-        templates: {
-          suggestion: function(o) { return '<div>' + o.name + '</div>'; }
-        }
-      });
-    </script>
-  EOC
+  erb :index
 end
 
 get '/stations' do
@@ -83,3 +44,47 @@ get '/queue' do
   content_type 'application/json'
   trains.to_json
 end
+
+__END__
+
+@@index
+
+<!DOCTYPE HTML>
+<title>
+  RESTful gateway for DSB Labs
+</title>
+<h1>
+  RESTful gateway for <a href="http://www.dsb.dk/dsb-labs/webservice-stationsafgange/">DSB Labs – Webservice: Stationsafgange</a>
+</h1>
+<p>
+  <em>All data is returned as JSON</em>
+</p>
+<p>
+  You might want to see the <a href="stations">list of stations</a>.
+</p>
+<form action="queue">
+  <p>
+    Or find departures etc. for a given station.
+    <label>Station UIC: <input type="text" name="uic"></label>
+    <input type="submit">
+  </p>
+</form>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js"></script>
+<script>
+  var stations = new Bloodhound({
+    identify: function(station) { return station.uic; },
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: '/stations'
+  });
+
+  $('input[name="uic"]').typeahead(null, {
+    display: 'uic',
+    name: 'stations',
+    source: stations,
+    templates: {
+      suggestion: function(o) { return '<div>' + o.name + '</div>'; }
+    }
+  });
+</script>
